29e8a9536ea32dda0774a8ce65b9ef5b075c69fe
Nkuthalo Makonco <118244106+nkuthalomakonco@users.noreply.github.com>
2023-09-09 08:56:22 +0200

    Add files via upload

---
+++ Mutex Class/Mutex Class/Module1.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Threading
+
+Module Module1
+    Private mut As Mutex = New Mutex()
+    Private Const numIterations As Integer = 1
+    Private Const numThreads As Integer = 3
+    Sub Main()
+        For index As Integer = 1 To numThreads
+            Dim Thread As Thread = New Thread(AddressOf ThreadProc)
+            Thread.Name = String.Format("Thread{0}", index)
+            Thread.Start()
+            'Console.WriteLine("Thread: {0}", index)
+        Next
+
+        'The main thread exits, but the application continues to
+        'run until all foreground threads have exited.
+        Console.ReadLine()
+    End Sub
+    Sub ThreadProc()
+        For index As Integer = 1 To numIterations
+            UseResource()
+        Next
+    End Sub
+    'This method represents a resource that must be synchronized
+    'so that only one thread at a time can enter.
+    Sub UseResource()
+        Console.WriteLine("{0} is requesting the mutex", Thread.CurrentThread.Name)
+        mut.WaitOne()
+        Console.WriteLine("{0} has entered the protected area", Thread.CurrentThread.Name)
+        'Place code to access non-reentrant resources here.
+
+        'Simulate some work.
+        Thread.Sleep(3000)
+        Console.WriteLine("{0} is leaving the protected area", Thread.CurrentThread.Name)
+        'Release the Mutex.
+        mut.ReleaseMutex()
+        Console.WriteLine("{0} has released the mutex", Thread.CurrentThread.Name)
+    End Sub
+
+End Module
---
+++ Mutex Class/Mutex Class/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Mutex Class")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Hewlett-Packard Company")>
+<Assembly: AssemblyProduct("Mutex Class")>
+<Assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("96347d56-d4b5-4f40-b83f-7028d7a6dd70")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
